7b7bd0fd64656c6acd09e914fb22f744119a0d47
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2024-03-26 16:31:16 -0300

    CannedChat

---
+++ src/request/requests/chat/CannedChat.ts
@@ -0,0 +1,13 @@
+import type IRequest from "../../../interfaces/request/IRequest.ts";
+import type Player from "../../../player/Player.ts";
+import type RequestArg from "../../RequestArg.ts";
+
+export default class CannedChat implements IRequest {
+
+	public readonly name: string = 'cc';
+
+	public async handler(player: Player, args: RequestArg): Promise<void> {
+		player.room?.writeArray("cc", args.getString(0), player.network.name);
+	}
+
+}
